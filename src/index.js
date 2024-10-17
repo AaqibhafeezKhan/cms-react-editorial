72c3d1e759d2d0e0b7364517d9557aa21c6ba03d
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-17 22:38:00 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PageEditor from "./PageEditor";
+
+function App() {
+  return <PageEditor />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
