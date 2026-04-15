9f09129e40515146b76b5eeffab25815adcf83ee
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 21:03:33 +0530

    build: configure webpack for SystemJS output

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,57 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'system',
-    publicPath: '',
+    publicPath: process.env.NODE_ENV === 'production' ? '/cms-react-editorial/' : '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
+        test: /\.(tsx?|jsx?)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                ['@babel/preset-react', { runtime: 'classic' }],
+                '@babel/preset-typescript',
+              ],
+            },
+          },
+        ],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: false,
+    }),
+  ],
   devServer: {
     port: 8081,
     historyApiFallback: true,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*',
     },
   },
-  externals: ["react", "react-dom"],
+  externals: isProduction
+    ? ['react', 'react-dom', 'single-spa-react']
+    : [],
 };
